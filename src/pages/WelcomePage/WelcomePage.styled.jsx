b2e366080549d1d7179ffc0ad7be220f3dcab842
DimaMen265 <menzak87@gmail.com>
2024-03-31 13:57:26 +0200

    Fix styles WP

--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -6,15 +6,19 @@ export const Container = styled.div`
   gap: 40px;
   justify-content: center;
   align-items: center;
-  padding: 24px 20px 40px;
+  padding-top: 24px;
+  padding-bottom: 40px;
 
   @media screen and (min-width: 768px) {
     gap: 60px;
+    padding-top: 40px;
+    padding-bottom: 50px;
   }
 
-  @media screen and (min-width: 1280px) {
+  @media screen and (min-width: 1440px) {
     flex-direction: row;
     align-items: end;
     gap: 80px;
+    padding: 80px 0;
   }
 `;

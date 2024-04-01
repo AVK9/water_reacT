b24533744bde6fbce367a77599ec19e142414bf3
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2024-04-01 18:47:16 +0300

    my commit description

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,20 +1,19 @@
-import styled from '@emotion/styled';
-import backgroundTop from './assets/img/background-main-page.png';
-import backgroundBottom from './assets/img/background-main-page-bottom.png';
+// import styled from '@emotion/styled';
+// import backgroundTop from './assets/img/background-main-page.png';
+// import backgroundBottom from './assets/img/background-main-page-bottom.png';
 import 'normalize.css';
 
-export const Background = styled.div`
-  width: 100%;
-  background-image: url(${backgroundTop});
-  background-repeat: no-repeat;
-  background-position: center top;
-  background-size: contain;
-  
-`;
-export const BackgroundBot = styled.div`
-  width: 100%;
-  background-image: url(${backgroundBottom});
-  background-repeat: no-repeat;
-  background-position: center bottom;
-  background-size: contain;
-`;
+// export const Background = styled.div`
+//   width: 100%;
+//   background-image: url(${backgroundTop});
+//   background-repeat: no-repeat;
+//   background-position: center top;
+//   background-size: contain;
+// `;
+// export const BackgroundBot = styled.div`
+//   width: 100%;
+//   background-image: url(${backgroundBottom});
+//   background-repeat: no-repeat;
+//   background-position: center bottom;
+//   background-size: contain;
+// `;
--- src/pages/SignInPage/SignInPage.styled.jsx
+++ src/pages/SignInPage/SignInPage.styled.jsx
@@ -1,57 +1,73 @@
 import styled from '@emotion/styled';
 
 export const SignInGlobalContainer = styled.section`
-    padding-top: 40px;
-    background-position: center center;
-    
-    background-size: cover;
-    background-repeat: no-repeat;
-    width: 100%;
-    
-`
+  padding-top: 24px;
+  display: flex;
+  flex-direction: column-reverse;
+`;
+  
+ 
+
 
 export const SignInContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    width: 100%;
-    align-items: flex-start;
-
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+  align-items: flex-start;
  
- `; 
+   background-image: url('/src/assets/img/Frame 18 mobail.png');
+`;
 
- 
- 
+export const SignInwater = styled.div`
 
+    background-image: url('/src/assets/img/mobile-woter.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 280px;
+    height: 210px;
+    background-position: center center;
+`
 
 export const SignInTitle = styled.h2`
-  margin-bottom: 20px;
+ font-weight: 500;
+ font-size: 26px;
+ line-height: 1.23077;
+ color: var(--primery-color-black);
+ margin-bottom: 20px;
 `;
 
 export const SignInLabel = styled.label`
   display: flex;
   gap: 16px;
   margin-bottom: 5px;
-  
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 1.33333;
+  color: var(--primery-color-black);
 `;
 
 export const SignInInput = styled.input`
-  border: 1px solid #d7e3ff;
+  border: 1px solid var(--secondary-color---5);
   border-radius: 6px;
   padding: 12px 10px;
   width: 280px;
   height: 44px;
-  background: #fff;
+  background: var(--primery-color-white);
+  outline: none;
+
+
+
 `;
 
 export const SignInButton = styled.button`
   width: 280px;
   height: 36px;
   padding: 10px;
-  
+
   box-shadow: 0 4px 8px 0 rgba(64, 123, 255, 0.34);
-  background: #407bff;
-  color: #fff;
+  background:var(--primery-color-blue);
+  color: var(--primery-color-white);
   border: none;
   border-radius: 10px;
   cursor: pointer;
@@ -60,8 +76,6 @@ export const SignInButton = styled.button`
     background-color: #0056b3;
   }
 `;
-
-
 
 export const TogglePasswordButton = styled.button`
   border: none;
@@ -72,5 +86,13 @@ export const TogglePasswordButton = styled.button`
   top: 50%;
   right: 10px;
   transform: translateY(-50%);
-  color:var(--primery-color-blue)
+   color: #76d7f0;
 `;
+
+export const ErrorMessage = styled.div`
+  color: red;
+  margin-top: 5px;
+ 
+`;
+
+

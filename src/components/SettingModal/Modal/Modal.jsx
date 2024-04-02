5a68e0ea5f2fd325228f87d022decdf2a3e45eb3
Sanjaba4585 <107932300+Sanjaba4585@users.noreply.github.com>
2024-04-02 17:53:06 +0200

    Modal form (#47)
    * Modal setting Form
    
    * add files

---
+++ src/components/SettingModal/Modal/Modal.jsx
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import sprite from '../../../assets/img/sprite.svg';
+import {
+  ButtonClose,
+  HeaderSettingModal,
+  HeaderSettingName,
+  ModalContent,
+  ModalOverlay,
+  WrapperModalContent,
+} from './Modal.styled';
+import SettingModalForm from '../SettingModalForm';
+import UploadImage from '../UploadImage/UploadImage';
+
+const Modal = ({ isOpen, children, onClose }) => {
+  useEffect(() => {
+    const close = (e) => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', close);
+    return () => {
+      document.removeEventListener('keydown', close);
+    };
+  }, [onClose]);
+
+  return (
+    <>
+      <ModalOverlay onClick={onClose}>
+        <ModalContent onClick={(e) => e.stopPropagation()}>
+          <HeaderSettingModal>
+            <HeaderSettingName>Setting</HeaderSettingName>
+            <ButtonClose onClick={onClose}>
+              <svg>
+                <use href={`${sprite}#icon-close`} />
+              </svg>
+            </ButtonClose>
+          </HeaderSettingModal>
+          <WrapperModalContent>
+            {children}
+            <UploadImage />
+            <SettingModalForm />
+          </WrapperModalContent>
+        </ModalContent>
+      </ModalOverlay>
+    </>
+  );
+};
+export default Modal;

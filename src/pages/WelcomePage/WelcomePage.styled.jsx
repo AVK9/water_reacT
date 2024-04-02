b7c59bced55faeffe90796390f42434b580b5a39
DimaMen265 <menzak87@gmail.com>
2024-04-02 03:52:33 +0200

    added adaptive background

--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -1,6 +1,57 @@
-import styled from '@emotion/styled';
+import styled from 'styled-components';
+import backgroundMobileTop from '../../assets/img/background-main-page-mobile-top.png';
+import backgroundMobileBottom from '../../assets/img/background-main-page-mobile-bottom.png';
+import backgroundTabletTop from '../../assets/img/background-main-page-tablet-top.png';
+import backgroundTabletBottom from '../../assets/img/background-main-page-tablet-bottom.png';
+import backgroundDesktopTop from '../../assets/img/background-main-page-desktop-top.png';
+import backgroundDesktopBottom from '../../assets/img/background-main-page-desktop-bottom.png';
 
-export const Container = styled.div`
+export const BackgroundTopWP = styled.div`
+  position: absolute;
+  top: 72px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-image: url(${backgroundMobileTop});
+  background-repeat: no-repeat;
+  background-position: center top;
+  background-size: contain;
+
+  @media screen and (min-width: 768px) {
+    top: 90px;
+    background-image: url(${backgroundTabletTop});
+  }
+
+  @media screen and (min-width: 1440px) {
+    top: 72px;
+    background-image: url(${backgroundDesktopTop});
+  }
+`;
+
+export const BackgroundBottomWP = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-image: url(${backgroundMobileBottom});
+  background-repeat: no-repeat;
+  background-position: center bottom;
+  background-size: contain;
+  
+
+  @media screen and (min-width: 768px) {
+    background-image: url(${backgroundTabletBottom});
+  }
+
+  @media screen and (min-width: 1440px) {
+    background-image: url(${backgroundDesktopBottom});
+  }
+`;
+
+export const ContainerWP = styled.div`
   display: flex;
   flex-direction: column;
   gap: 40px;
@@ -8,6 +59,11 @@ export const Container = styled.div`
   align-items: center;
   padding-top: 24px;
   padding-bottom: 40px;
+
+  @media screen and (max-width: 520px) {
+    padding-right: 20px;
+    padding-left: 20px;
+  }
 
   @media screen and (min-width: 768px) {
     gap: 60px;

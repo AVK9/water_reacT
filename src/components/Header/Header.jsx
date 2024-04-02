a8a8dd11c0c8d7ae8b0a62d11d6dccc284c22bf5
AlexKat <64771146+AVK9@users.noreply.github.com>
2024-04-03 01:41:22 +0300

    Q (#52)
    * BGROK
    
    * Section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,17 +2,18 @@ import { HeaderContainer, Navigation } from './Header.styled';
 import Logo from './../Logo/Logo';
 import UserAuth from '../UserAuth/UserAuth';
 import HeaderSetting from '../SettingModal/HeaderSetting/HeaderSetting';
-
+import { Section } from '../Section/Section';
 
 export const Header = () => {
   return (
-    <HeaderContainer>
-      <Navigation>
-        <Logo />
-        <UserAuth /> 
-      </Navigation>
-      <HeaderSetting />
-      
-    </HeaderContainer>
+    <Section>
+      <HeaderContainer>
+        <Navigation>
+          <Logo />
+          <UserAuth />
+        </Navigation>
+        <HeaderSetting />
+      </HeaderContainer>
+    </Section>
   );
 };

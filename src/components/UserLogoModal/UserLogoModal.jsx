e4ed691b708a9bc82731a942907e565e9fcfd6bf
DimaMen265 <menzak87@gmail.com>
2024-04-04 16:06:34 +0200

    added logout modal

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import Modal from '../SettingModal/Modal/Modal';
-import { ContainerUserLogoModal, ButtonsUserLogoModal, IconWrapper, ButtonText } from './UserLogoModal.styled';
+import UserLogoutModal from '../UserLogoutModal/UserLogoutModal';
+import {
+  ContainerUserLogoModal,
+  ButtonsUserLogoModal,
+  IconWrapper,
+ ButtonText
+} from './UserLogoModal.styled';
 import sprite from '../../assets/img/sprite.svg';
 
 const UserLogoModal = () => {
@@ -8,23 +14,33 @@ const UserLogoModal = () => {
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = (setIsModalOpen) => setIsModalOpen(false);
 
+  const [isModalLogoutOpen, setIsModalLogoutOpen] = useState(false);
+  const handleOpenModalLogout = () => setIsModalLogoutOpen(true);
+  const handleCloseModalLogout = () => setIsModalLogoutOpen(false);
+
   return (
     <ContainerUserLogoModal>
-      <ButtonsUserLogoModal onClick={handleOpenModal}>
+      <ButtonsUserLogoModal type='button' onClick={handleOpenModal}>
         <IconWrapper>
           <use xlinkHref={`${sprite}#icon-cog-6-tooth`}></use>
         </IconWrapper>
+
         <ButtonText>Setting</ButtonText>
       </ButtonsUserLogoModal>
-      <ButtonsUserLogoModal>
+
+      <ButtonsUserLogoModal type='button' onClick={handleOpenModalLogout}>
         <IconWrapper>
           <use xlinkHref={`${sprite}#icon-arrow-right-on-rectangle`}></use>
         </IconWrapper>
+
         <ButtonText>Log out</ButtonText>
       </ButtonsUserLogoModal>
+
       <Modal isOpen={isModalOpen}
         onClose={() =>handleCloseModal(setIsModalOpen) }>
       </Modal>
+      
+      {isModalLogoutOpen && <UserLogoutModal onClose={handleCloseModalLogout} />}
     </ContainerUserLogoModal>
   )
 };

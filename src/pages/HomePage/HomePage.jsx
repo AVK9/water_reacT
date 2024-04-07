0b468f8de599d3cb496be6a5161a863578ac2855
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2024-04-08 01:57:11 +0300

    WaterRatioPanel

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,12 +7,14 @@ import {
 } from './HomePage.styled';
 import TodayWaterList from '../../components/TodayWaterList/TodayWaterList';
 import { Section } from '../../components/Section/Section';
+import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel';
 const HomePage = () => {
   return (
     <Background>
       <BackgroundBottom>
         <Section>
           <DailyNorma />
+          <WaterRatioPanel/> 
           <WaterControlBox>
             <TodayWaterList />
             <MonthStatsTable />

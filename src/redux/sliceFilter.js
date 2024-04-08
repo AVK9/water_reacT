ba267f3feb73d752a5b8c9145875304a5dfbf1b4
pavloit <dba.devop@gmail.com>
2024-04-08 18:41:06 +0300

    sliceFilter file restore

---
+++ src/redux/sliceFilter.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    filter: "",
+}
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+        filterContactAction: (state, { payload }) => {
+            state.filter = payload
+        }
+    }
+})
+
+export const { filterContactAction } = filterSlice.actions
+export const filterReduser = filterSlice.reducer

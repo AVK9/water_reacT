1650647da19ff5f0a43d6ef953876974da19a81d
DimaMen265 <134741876+DimaMen265@users.noreply.github.com>
2024-04-09 03:10:26 +0200

    Added water modal (#136)
    * added addWaterModal
    
    * fix date

---
+++ src/components/AddWaterModal/AddWaterModal.jsx
@@ -0,0 +1,191 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addWaterThunk } from '../../redux/water/waterThunk';
+import {
+  Backdrop,
+  ModalContainer,
+  HeaderAddModal,
+  ModalTitle,
+  CloseButton,
+  ChooseValueText,
+  AmountOfWaterText,
+  WaterAmountControls,
+  WaterAmountButton,
+  WaterAmountInput,
+  IconDispenser,
+  TimeContainer,
+  StyledSelect,
+  EnterWaterValueText,
+  WaterAmountInputManually,
+  AddWaterAmountContainer,
+  AddWaterAmount,
+  SaveButton,
+  IconWrapper
+} from './AddWaterModal.styled';
+import sprite from '../../assets/img/sprite.svg';
+import * as dateFns from 'date-fns';
+
+const formatTime = (date) => {
+  const hours = dateFns.format(date, 'HH');
+  const minutes = dateFns.format(date, 'mm');
+  return `${hours}:${minutes}`;
+};
+
+const getTimeOptions = (start, end, step = 5) => {
+  const options = [];
+  let currentTime = start;
+  const endTime = end;
+
+  while (currentTime <= endTime) {
+    const timeString = formatTime(currentTime);
+    options.push({ value: timeString, label: timeString });
+    currentTime = dateFns.add(currentTime, { minutes: step });
+  }
+
+  return options;
+};
+
+const AddWaterModal = ({ initialValue = 50, onClose }) => {
+    const [waterAmount, setWaterAmount] = useState(initialValue);
+    const [selectedTime, setSelectedTime] = useState(new Date());
+
+    const handleTimeChange = (e) => {
+        const [hours, minutes] = e.target.value.split(':');
+        const newDate = new Date();
+        newDate.setHours(hours);
+        newDate.setMinutes(minutes);
+        setSelectedTime(newDate);
+    };
+
+    const now = new Date();
+    const adjustedTime = dateFns.sub(now, { minutes: -180 });
+
+    const timeOptions = getTimeOptions(new Date(0, 0, 0, 0, 0), new Date(0, 0, 0, 23, 59), 5);
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClose = () => {
+        setIsOpen(false);
+        setTimeout(onClose, 500);
+    };
+
+    const handleBackdropClick = (event) => {
+        if (event.currentTarget === event.target) {
+            handleClose();
+        }
+    };
+
+    useEffect(() => {
+        setIsOpen(true);
+        document.body.style.overflow = 'hidden';
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, []);
+
+    useEffect(() => {
+        const close = (e) => {
+            if (e.code === 'Escape') {
+                handleClose();
+            }
+        };
+    
+        document.addEventListener('keydown', close);
+    
+        return () => {
+            document.removeEventListener('keydown', close);
+        };
+    }, [onClose]);
+
+    const handleIncrement = () => {
+        setWaterAmount((prevAmount) => Number(prevAmount) + 50);
+    };
+
+    const handleDecrement = () => {
+        setWaterAmount((prevAmount) => (Number(prevAmount) >= 50 ? Number(prevAmount) - 50 : 0));
+    };
+
+    const dispatch = useDispatch();
+
+    const handleAddWater = () => {
+        dispatch(addWaterThunk({ date: adjustedTime, waterAmount: waterAmount }));
+        handleClose();
+    };
+
+    return (
+        <Backdrop isOpen={isOpen} onClick={handleBackdropClick}>
+            <ModalContainer isOpen={isOpen}>
+                <HeaderAddModal>
+                    <ModalTitle>Add water</ModalTitle>
+
+                    <CloseButton onClick={handleClose}>
+                        <IconWrapper>
+                            <use href={`${sprite}#icon-close`} />
+                        </IconWrapper>
+                    </CloseButton>
+                </HeaderAddModal>
+
+                <div>
+                    <ChooseValueText>Choose a value:</ChooseValueText>
+                    <AmountOfWaterText>Amount of water:</AmountOfWaterText>
+
+                    <WaterAmountControls>
+                        <WaterAmountButton onClick={handleDecrement}>
+                            <IconDispenser>
+                                <use href={`${sprite}#icon-minus-small`} />
+                            </IconDispenser>
+                        </WaterAmountButton>
+
+                        <WaterAmountInput value={waterAmount + ' ml'} readOnly={true} />
+
+                        <WaterAmountButton onClick={handleIncrement}>
+                            <IconDispenser>
+                                <use href={`${sprite}#icon-plus-small`} />
+                            </IconDispenser>
+                        </WaterAmountButton>
+                    </WaterAmountControls>
+                </div>
+
+                <div>
+                    <AmountOfWaterText>Recording time:</AmountOfWaterText>
+
+                    <TimeContainer>
+                        <div>
+                            <StyledSelect
+                                id="time"
+                                value={formatTime(selectedTime)}
+                                onChange={handleTimeChange}
+                            >
+                                {timeOptions.map(({ value, label }) => (
+                                    <option key={value} value={value}>
+                                        {label}
+                                    </option>
+                                ))}
+                            </StyledSelect>
+                        </div>
+                    </TimeContainer>
+                </div>
+
+                <div>
+                    <EnterWaterValueText>Enter the value of the water used:</EnterWaterValueText>
+
+                    <WaterAmountInputManually
+                        value={waterAmount + ' ml' || '0'}
+                        onChange={(e) => {
+                            const newValue = parseInt(e.target.value.replace(/\D/g, ''));
+                            setWaterAmount(isNaN(newValue) || e.target.value === '' ? 0 : newValue);
+                        }}
+                    />
+                </div>
+
+                <AddWaterAmountContainer>
+                    <AddWaterAmount>{waterAmount || '0'} ml</AddWaterAmount>
+                    
+                    <SaveButton onClick={handleAddWater}>Save</SaveButton>
+                </AddWaterAmountContainer>
+            </ModalContainer>
+        </Backdrop>
+    );
+};
+
+export default AddWaterModal;

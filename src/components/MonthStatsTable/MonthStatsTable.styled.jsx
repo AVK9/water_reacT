4374e10293a0191f0e01f88ad9b28e81ac1d39ab
AlexKat <katsubo79@gmail.com>
2024-04-10 23:25:43 +0300

    WaterRateFFF

--- src/components/MonthStatsTable/MonthStatsTable.styled.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.styled.jsx
@@ -96,9 +96,9 @@ export const CalendarData = styled.div`
   border-radius: 50%;
   border: 1px solid
     ${({ percentage }) =>
-      percentage === 100
-        ? 'var(--primery-color-white)'
-        : 'var(--secondary-color-5)'};
+      percentage < 100
+        ? 'var(--secondary-color-5)'
+        : 'var(--primery-color-white)'};
   background: var(--primery-color-white);
   display: flex;
   align-items: center;

c1d918596e8afc395dd4b41a37543e7508c9bb1a
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2024-04-10 21:14:42 +0300

    toglebtn

--- src/redux/water/waterSelectors.js
+++ src/redux/water/waterSelectors.js
@@ -15,3 +15,18 @@ export const selectVisibleDrinking = createSelector(
       : dayWaterList;
   }
 );
+
+
+
+
+
+
+
+export const selectDailyNormFulfillment = state => {
+  const selectDay = selectSelectDay(state);
+  const dailyWaterConsumption = selectDay?.waterConsumption || 1500; 
+  const dailyNorm = 2000; 
+  return (dailyWaterConsumption / dailyNorm) * 100;
+};
+
+
